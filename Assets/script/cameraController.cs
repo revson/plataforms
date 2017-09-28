1a339c25965c24bf2ed326fa952cfd387bee91e1
revson <revson@gmail.com>
2017-09-28 17:23:57 -0300

    add arquivos projeto

---
+++ Assets/script/cameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+	private player scPlayer; //script Player
+	public Transform L,R; // limite da camera no eixo X
+	private Vector3 destinyX, destinyY;
+	public float speed, cameraHeight;
+
+	// Use this for initialization
+	void Start () {
+
+		scPlayer = FindObjectOfType (typeof(player)) as player;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// configurando o destino X e Y da camera
+		destinyX = new Vector3 (scPlayer.transform.position.x, transform.position.y, transform.position.z );
+		destinyY = new Vector3 (transform.position.x, scPlayer.transform.position.y + cameraHeight, transform.position.z );
+
+		//aplicando acompanhamento da camera no eixo Y suavizado
+		transform.position = Vector3.Lerp (transform.position, destinyY, speed * Time.deltaTime);
+
+		//estabelecendo limite da camera e aplicando acompanhamento da camera no eixo X suavizado no personagem
+		if(scPlayer.transform.position.x > L.position.x && scPlayer.transform.position.x < R.position.x){
+			transform.position = Vector3.Lerp (transform.position, destinyX, speed * Time.deltaTime);
+		}else if(transform.position.x > L.position.x && transform.position.x < R.position.x){
+			transform.position = Vector3.Lerp (transform.position, destinyX, speed * Time.deltaTime);
+		}
+
+	}
+}
---
+++ Assets/script/player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	//variaveis
+	private Rigidbody2D rb;
+
+	private float horizontal;
+	public float speed;
+	private bool faceRight;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+
+		//inicializa com o valor do scale se for > 0 (olhando para direita)
+		faceRight = transform.localScale.x > 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		//horizontal recebe o valor do eixo X positivo para direita e negetivo para esquerda
+		horizontal = Input.GetAxisRaw ("Horizontal");
+		move (horizontal);
+		flip (horizontal);
+	}
+
+	private void move(float axisX){
+		rb.velocity = new Vector2(axisX * speed, rb.velocity.y) ;
+	}
+
+	private void flip (float axisX){
+	
+		//se o eixo x for maior que 0 e a face para esquerda ou o eixo x < 0 e a face para direita
+		if(axisX > 0 && !faceRight || axisX < 0 && faceRight){
+
+			//se retorno for verdedadeiro vira a face para a direcao do eixo
+			//a face recebe ela ao contrario, ficando assim na posicao correta
+			faceRight = !faceRight;
+			//ao passar o negativo para o localScale, ele corrige o lado do sprite fazendo a regra do sinal
+			transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z );
+		}
+	}
+}
